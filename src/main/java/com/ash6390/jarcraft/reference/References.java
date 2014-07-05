b6c14b3657580881b5659b6c419fe2c3e711b347
Ash6390 <matteo.colombi259@gmail.com>
2014-07-05 16:54:25 +0200

    Added GUI_FACTORY_CLASS

--- src/main/java/com/ash6390/jarcraft/reference/References.java
+++ src/main/java/com/ash6390/jarcraft/reference/References.java
@@ -7,4 +7,5 @@ public class References
     public static final String VERSION = "1.7.2-1.3";
     public static final String CLIENT_PROXY_CLASS = "com.ash6390.jarcraft.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.ash6390.jarcraft.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "com.ash6390.jarcraft.client.gui.GuiFactory";
 }
