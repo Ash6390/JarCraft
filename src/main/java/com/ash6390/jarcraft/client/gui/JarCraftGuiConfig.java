c49557782c70c5676cfb2b78e3bbb4f118069b57
Ash6390 <matteo.colombi259@gmail.com>
2014-07-05 16:53:24 +0200

    New Configuration GUI! Thanks bspkrs.

---
+++ src/main/java/com/ash6390/jarcraft/client/gui/JarCraftGuiConfig.java
@@ -0,0 +1,19 @@
+package com.ash6390.jarcraft.client.gui;
+
+import com.ash6390.jarcraft.handler.ConfigurationHandler;
+import com.ash6390.jarcraft.reference.References;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+public class JarCraftGuiConfig extends GuiConfig
+{
+    public JarCraftGuiConfig(GuiScreen guiScreen)
+    {
+        super(guiScreen,
+                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                References.MODID, false, false, GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+
+    }
+}
