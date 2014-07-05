963826c506f0aec541052ac122da49dbb14cac7f
Ash6390 <matteo.colombi259@gmail.com>
2014-07-05 16:55:18 +0200

    Modified Material from Ground to Iron

--- src/main/java/com/ash6390/jarcraft/blocks/JarCraftBlocks.java
+++ src/main/java/com/ash6390/jarcraft/blocks/JarCraftBlocks.java
@@ -5,6 +5,7 @@ import com.ash6390.jarcraft.reference.References;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.audio.SoundRegistry;
 
 public class JarCraftBlocks
 {
