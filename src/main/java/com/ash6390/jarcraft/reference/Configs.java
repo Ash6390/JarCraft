04c9428976075d79b0c7049fe2e797ba4c1e4d07
Ash6390 <matteo.colombi259@gmail.com>
2014-07-07 18:50:42 +0200

    Added new enable Items/Blocks config.

--- src/main/java/com/ash6390/jarcraft/reference/Configs.java
+++ src/main/java/com/ash6390/jarcraft/reference/Configs.java
@@ -2,7 +2,7 @@ package com.ash6390.jarcraft.reference;
 
 public class Configs
 {
-    public static boolean uselessValue = false;
-    public static double uselessDouble = 0.0;
-    public static String uselessString = "Hello!";
+
+    public static boolean itemsOn;
+
 }
