6d236bb2fcb81ed08982b56c0a28a6925532324b
Ash6390 <matteo.colombi259@gmail.com>
2014-07-07 18:51:13 +0200

    Changes for the new way of testing items AND new Config

--- src/main/java/com/ash6390/jarcraft/JarCraftCore.java
+++ src/main/java/com/ash6390/jarcraft/JarCraftCore.java
@@ -2,8 +2,10 @@ package com.ash6390.jarcraft;
 
 import com.ash6390.jarcraft.blocks.JarCraftBlocks;
 import com.ash6390.jarcraft.handler.ConfigurationHandler;
+import com.ash6390.jarcraft.init.ModItems;
 import com.ash6390.jarcraft.items.JarCraftItem;
 import com.ash6390.jarcraft.proxy.ServerProxy;
+import com.ash6390.jarcraft.reference.Configs;
 import com.ash6390.jarcraft.reference.References;
 import com.ash6390.jarcraft.utility.LogHelper;
 import cpw.mods.fml.common.FMLCommonHandler;
@@ -13,6 +15,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 
 @Mod(modid=References.MODID, name=References.NAME, version = References.VERSION, guiFactory = References.GUI_FACTORY_CLASS)
@@ -31,18 +34,38 @@ public class JarCraftCore
     {
         public Item getTabIconItem()
         {
-            return JarCraftItem.glassIngot;
+            if (Configs.itemsOn)
+            {
+                return JarCraftItem.glassIngot;
+
+            } else {
+
+                return Items.glass_bottle;
+
+            }
+
         }
     };
 
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
+
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+
         FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
-        JarCraftItem.ItemInit();
-        JarCraftBlocks.BlockInit();
-        Recipes.registerRecipes();
+
+        if (Configs.itemsOn)
+        {
+
+            JarCraftBlocks.BlockInit();
+            JarCraftItem.ItemInit();
+            Recipes.registerRecipes();
+
+            ModItems.init();
+
+        }
+
         LogHelper.info("Pre Initialization Complete!");
     }
 
