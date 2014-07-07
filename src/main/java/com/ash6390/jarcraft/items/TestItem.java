766ce1c39a1564a7b360740ecf27406106f929f7
Ash6390 <matteo.colombi259@gmail.com>
2014-07-07 18:50:02 +0200

    Can you guess? No? Testing out a new way to make items!

---
+++ src/main/java/com/ash6390/jarcraft/items/TestItem.java
@@ -0,0 +1,10 @@
+package com.ash6390.jarcraft.items;
+
+public class testItem extends ItemJC
+{
+    public testItem()
+    {
+        super();
+        this.setUnlocalizedName("testItem");
+    }
+}
