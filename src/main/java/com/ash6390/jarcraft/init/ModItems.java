f3d43c3b96a15455e612b0d0ea903feb0a196a21
Ash6390 <matteo.colombi259@gmail.com>
2014-07-09 14:39:54 +0200

    New Items for the recipe of the new block (may change)

--- src/main/java/com/ash6390/jarcraft/init/ModItems.java
+++ src/main/java/com/ash6390/jarcraft/init/ModItems.java
@@ -1,15 +1,20 @@
 package com.ash6390.jarcraft.init;
 
-import com.ash6390.jarcraft.items.ItemJC;
-import com.ash6390.jarcraft.items.testItem;
+import com.ash6390.jarcraft.items.*;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModItems
 {
-    public static final ItemJC testItem = new testItem();
+    public static final ItemJC testItem = new TestItem();
+    public static final ItemJC illuminatingOrb = new IlluminatingOrb();
+    public static final ItemJC glassIngot = new GlassIngot();
+    public static final ItemJC glassStick = new GlassStick();
 
     public static void init()
     {
         GameRegistry.registerItem(testItem, "testItem");
+        GameRegistry.registerItem(illuminatingOrb, "illuminatingOrb");
+        GameRegistry.registerItem(glassIngot, "glassIngot");
+        GameRegistry.registerItem(glassStick, "glassStick");
     }
 }
