751395eb459e61e7254e5dc45e6ad2444d637bf2
Ash6390 <matteo.colombi259@gmail.com>
2014-07-09 14:42:49 +0200

    Added recipe for the glass lamp

--- src/main/java/com/ash6390/jarcraft/Recipes.java
+++ src/main/java/com/ash6390/jarcraft/Recipes.java
@@ -1,10 +1,11 @@
 package com.ash6390.jarcraft;
 
-import com.ash6390.jarcraft.blocks.JarCraftBlocks;
+import com.ash6390.jarcraft.init.ModBlocks;
+import com.ash6390.jarcraft.init.ModItems;
 import com.ash6390.jarcraft.items.JarCraftItem;
-import com.ash6390.jarcraft.reference.Configs;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -13,98 +14,98 @@ public class Recipes
     public static void registerRecipes()
     {
         //Glass Ingot
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 2), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 2), new Object[]{
                 "IG ",
                 "GI ",
                 "   ", 'I', Item.itemRegistry.getObject("iron_ingot"), 'G', Block.blockRegistry.getObject("glass")});
 
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 2), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 2), new Object[]{
                 " IG",
                 " GI",
                 "   ", 'I', Item.itemRegistry.getObject("iron_ingot"), 'G', Block.blockRegistry.getObject("glass")});
 
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 2), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 2), new Object[]{
                 "   ",
                 "IG ",
                 "GI ", 'I', Item.itemRegistry.getObject("iron_ingot"), 'G', Block.blockRegistry.getObject("glass")});
 
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 2), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 2), new Object[]{
                 "   ",
                 " IG",
                 " GI", 'I', Item.itemRegistry.getObject("iron_ingot"), 'G', Block.blockRegistry.getObject("glass")});
 
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 9), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 9), new Object[]{
                     "B  ",
                     "   ",
-                    "   ", 'B', JarCraftBlocks.GlassIngotBlock});
+                    "   ", 'B', ModBlocks.glassIngotBlock});
 
-            GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 9), new Object[]{
+            GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 9), new Object[]{
                     " B ",
                     "   ",
-                    "   ", 'B', JarCraftBlocks.GlassIngotBlock});
+                    "   ", 'B', ModBlocks.glassIngotBlock});
 
-            GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 9), new Object[]{
+            GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 9), new Object[]{
                     "  B",
                     "   ",
-                    "   ", 'B', JarCraftBlocks.GlassIngotBlock});
+                    "   ", 'B', ModBlocks.glassIngotBlock});
 
-            GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 9), new Object[]{
+            GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 9), new Object[]{
                     "   ",
                     "B  ",
-                    "   ", 'B', JarCraftBlocks.GlassIngotBlock});
+                    "   ", 'B', ModBlocks.glassIngotBlock});
 
-            GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 9), new Object[]{
+            GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 9), new Object[]{
                     "   ",
                     " B ",
-                    "   ", 'B', JarCraftBlocks.GlassIngotBlock});
+                    "   ", 'B', ModBlocks.glassIngotBlock});
 
-            GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 9), new Object[]{
+            GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 9), new Object[]{
                     "   ",
                     "  B",
-                    "   ", 'B', JarCraftBlocks.GlassIngotBlock});
+                    "   ", 'B', ModBlocks.glassIngotBlock});
 
-            GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 9), new Object[]{
+            GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 9), new Object[]{
                     "   ",
                     "   ",
-                    "B  ", 'B', JarCraftBlocks.GlassIngotBlock});
+                    "B  ", 'B', ModBlocks.glassIngotBlock});
 
-            GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 9), new Object[]{
+            GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 9), new Object[]{
                     "   ",
                     "   ",
-                    " B ", 'B', JarCraftBlocks.GlassIngotBlock});
+                    " B ", 'B', ModBlocks.glassIngotBlock});
 
-            GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassIngot, 9), new Object[]{
+            GameRegistry.addRecipe(new ItemStack(ModItems.glassIngot, 9), new Object[]{
                     "   ",
                     "   ",
-                    "  B", 'B', JarCraftBlocks.GlassIngotBlock});
+                    "  B", 'B', ModBlocks.glassIngotBlock});
 
         //Glass Stick
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassStick, 4), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassStick, 4), new Object[]{
                 "G  ",
                 "G  ",
                 "   ", 'G', Block.blockRegistry.getObject("glass")});
 
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassStick, 4), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassStick, 4), new Object[]{
                 " G ",
                 " G ",
                 "   ", 'G', Block.blockRegistry.getObject("glass")});
 
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassStick, 4), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassStick, 4), new Object[]{
                 "  G",
                 "  G",
                 "   ", 'G', Block.blockRegistry.getObject("glass")});
 
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassStick, 4), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassStick, 4), new Object[]{
                 "   ",
                 "G  ",
                 "G  ", 'G', Block.blockRegistry.getObject("glass")});
 
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassStick, 4), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassStick, 4), new Object[]{
                 "   ",
                 " G ",
                 " G ", 'G', Block.blockRegistry.getObject("glass")});
 
-        GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassStick, 4), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModItems.glassStick, 4), new Object[]{
                 "   ",
                 "  G",
                 "  G", 'G', Block.blockRegistry.getObject("glass")});
@@ -113,97 +114,109 @@ public class Recipes
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassSword, 1), new Object[]{
                 " I ",
                 " I ",
-                " S ", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                " S ", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassSword, 1), new Object[]{
                 "I  ",
                 "I  ",
-                "S  ", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                "S  ", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassSword, 1), new Object[]{
                 "  I",
                 "  I",
-                "  S", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                "  S", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         //Glass Pickaxe
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassPick, 1), new Object[]{
                 "III",
                 " S ",
-                " S ", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                " S ", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         //Glass Shovel
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassShovel, 1), new Object[]{
                 " I ",
                 " S ",
-                " S ", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                " S ", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassShovel, 1), new Object[]{
                 "I  ",
                 "S  ",
-                "S  ", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                "S  ", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassShovel, 1), new Object[]{
                 "  I",
                 "  S",
-                "  S", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                "  S", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         //Glass Axe
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassAxe, 1), new Object[]{
                 "II ",
                 "IS ",
-                " S ", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                " S ", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassAxe, 1), new Object[]{
                 " II",
                 " SI",
-                " S ", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                " S ", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         //Glass Hoe
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassHoe, 1), new Object[]{
                 "II ",
                 " S ",
-                " S ", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                " S ", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassHoe, 1), new Object[]{
                 " II",
                 " S ",
-                " S ", 'I', JarCraftItem.glassIngot, 'S', JarCraftItem.glassStick});
+                " S ", 'I', ModItems.glassIngot, 'S', ModItems.glassStick});
 
         //Glass Helmet
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassHelmet, 1), new Object[]{
                 "III",
                 "I I",
-                "   ", 'I', JarCraftItem.glassIngot});
+                "   ", 'I', ModItems.glassIngot});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassHelmet, 1), new Object[]{
                 "   ",
                 "III",
-                "I I", 'I', JarCraftItem.glassIngot});
+                "I I", 'I', ModItems.glassIngot});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassChestplate, 1), new Object[]{
                 "I I",
                 "III",
-                "III", 'I', JarCraftItem.glassIngot});
+                "III", 'I', ModItems.glassIngot});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassLeggings, 1), new Object[]{
                 "III",
                 "I I",
-                "I I", 'I', JarCraftItem.glassIngot});
+                "I I", 'I', ModItems.glassIngot});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassBoots, 1), new Object[]{
                 "   ",
                 "I I",
-                "I I", 'I', JarCraftItem.glassIngot});
+                "I I", 'I', ModItems.glassIngot});
 
         GameRegistry.addRecipe(new ItemStack(JarCraftItem.glassBoots, 1), new Object[]{
                 "I I",
                 "I I",
-                "   ", 'I', JarCraftItem.glassIngot});
+                "   ", 'I', ModItems.glassIngot});
+
+        //Illuminating Orb
+        GameRegistry.addRecipe(new ItemStack(ModItems.illuminatingOrb, 8), new Object[]{
+                "GGG",
+                "GIG",
+                "GGG", 'G', Blocks.glowstone, 'I', ModItems.glassIngot});
 
         //Glass Ingot Block
-        GameRegistry.addRecipe(new ItemStack(JarCraftBlocks.GlassIngotBlock, 1), new Object[]{
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.glassIngotBlock, 1), new Object[]{
                 "III",
                 "III",
-                "III", 'I', JarCraftItem.glassIngot});
+                "III", 'I', ModItems.glassIngot});
+
+        //Glass Lamp
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.glassLamp, 1), new Object[] {
+                "GGG",
+                "GOG",
+                "GGG", 'G', Blocks.glass, 'O', ModItems.illuminatingOrb});
     }
 }
