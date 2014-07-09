2bec7c6808b225d8fc7f98a593217cbd8a9f69b8
Ash6390 <matteo.colombi259@gmail.com>
2014-07-09 14:40:21 +0200

    Modified to the new item class.

---
+++ src/main/java/com/ash6390/jarcraft/items/GlassIngot.java
@@ -0,0 +1,13 @@
+package com.ash6390.jarcraft.items;
+
+import com.ash6390.jarcraft.JarCraftCore;
+
+public class GlassIngot extends ItemJC
+{
+    public GlassIngot()
+    {
+        super();
+        this.setUnlocalizedName("glassIngot");
+        this.setCreativeTab(JarCraftCore.JarCraftTab);
+    }
+}
---
+++ src/main/java/com/ash6390/jarcraft/items/GlassStick.java
@@ -0,0 +1,13 @@
+package com.ash6390.jarcraft.items;
+
+import com.ash6390.jarcraft.JarCraftCore;
+
+public class GlassStick extends ItemJC
+{
+    public GlassStick()
+    {
+        super();
+        this.setUnlocalizedName("glassStick");
+        this.setCreativeTab(JarCraftCore.JarCraftTab);
+    }
+}
