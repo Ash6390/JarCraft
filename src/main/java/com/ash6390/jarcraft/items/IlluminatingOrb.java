9a539d826c8c00858c1e8e6745895b8165ebde60
Ash6390 <matteo.colombi259@gmail.com>
2014-07-09 14:40:38 +0200

    Crafting material for the new Block

---
+++ src/main/java/com/ash6390/jarcraft/items/IlluminatingOrb.java
@@ -0,0 +1,13 @@
+package com.ash6390.jarcraft.items;
+
+import com.ash6390.jarcraft.JarCraftCore;
+
+public class IlluminatingOrb extends ItemJC
+{
+    public IlluminatingOrb()
+    {
+        super();
+        this.setUnlocalizedName("illuminatingOrb");
+        this.setCreativeTab(JarCraftCore.JarCraftTab);
+    }
+}
