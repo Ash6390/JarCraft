67e0f3bfff4920b4d81fcb5f0a788e714d046d24
Ash6390 <matteo.colombi259@gmail.com>
2014-07-09 14:37:48 +0200

    New Block class (making blocks in a different way)

---
+++ src/main/java/com/ash6390/jarcraft/blocks/BlockJC.java
@@ -0,0 +1,39 @@
+package com.ash6390.jarcraft.blocks;
+
+import com.ash6390.jarcraft.reference.References;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+public class BlockJC extends Block
+{
+    public BlockJC(Material material)
+    {
+        super(material);
+    }
+
+    public BlockJC()
+    {
+        this(Material.rock);
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("tile.%s%s", References.MODID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        blockIcon = iconRegister.registerIcon(String.format("%s", getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+}
