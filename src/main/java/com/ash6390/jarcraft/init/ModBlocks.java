87362cf1e9582bd920e09fa9285da5b356e8fa58
Ash6390 <matteo.colombi259@gmail.com>
2014-07-12 14:47:05 +0200

    Changed to work with the new Creative Tab

--- src/main/java/com/ash6390/jarcraft/init/ModBlocks.java
+++ src/main/java/com/ash6390/jarcraft/init/ModBlocks.java
@@ -1,9 +1,9 @@
 package com.ash6390.jarcraft.init;
 
-import com.ash6390.jarcraft.JarCraftCore;
 import com.ash6390.jarcraft.blocks.BlockJC;
 import com.ash6390.jarcraft.blocks.GlassIngotBlock;
 import com.ash6390.jarcraft.blocks.GlassLamp;
+import com.ash6390.jarcraft.creativetab.CreativeTabJC;
 import com.ash6390.jarcraft.reference.References;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
@@ -13,7 +13,7 @@ import net.minecraft.block.material.Material;
 public class ModBlocks
 {
     public static final BlockJC glassIngotBlock = new GlassIngotBlock();
-    public static final Block glassLamp = new GlassLamp(Material.glass).setLightLevel(1.0F).setBlockName("glassLamp").setHardness(0.3F).setResistance(0.3F).setCreativeTab(JarCraftCore.JarCraftTab).setBlockTextureName("glassLamp");
+    public static final Block glassLamp = new GlassLamp(Material.glass).setLightLevel(1.0F).setBlockName("glassLamp").setHardness(0.3F).setResistance(0.3F).setCreativeTab(CreativeTabJC.JC_TAB).setBlockTextureName("glassLamp");
 
     public static void init()
     {
