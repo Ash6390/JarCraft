d4c40594a48356d1694fe0fdfc3db0e730cb0ac5
Ash6390 <matteo.colombi259@gmail.com>
2014-07-12 14:47:13 +0200

    Changed to work with the new Creative Tab

--- src/main/java/com/ash6390/jarcraft/init/ModItems.java
+++ src/main/java/com/ash6390/jarcraft/init/ModItems.java
@@ -5,14 +5,12 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModItems
 {
-    public static final ItemJC testItem = new TestItem();
     public static final ItemJC illuminatingOrb = new IlluminatingOrb();
     public static final ItemJC glassIngot = new GlassIngot();
     public static final ItemJC glassStick = new GlassStick();
 
     public static void init()
     {
-        GameRegistry.registerItem(testItem, "testItem");
         GameRegistry.registerItem(illuminatingOrb, "illuminatingOrb");
         GameRegistry.registerItem(glassIngot, "glassIngot");
         GameRegistry.registerItem(glassStick, "glassStick");
