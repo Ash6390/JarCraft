34142f351831120d0066f31f41767dd3be4b83be
Matteo <matteo.colombi259@gmail.com>
2014-10-17 22:49:48 +0200

    Fixed Missing Texture and added illuminating orb

--- src/main/java/com/ash6390/jarcraft/proxy/ClientProxy.java
+++ src/main/java/com/ash6390/jarcraft/proxy/ClientProxy.java
@@ -2,22 +2,29 @@ package com.ash6390.jarcraft.proxy;
 
 import com.ash6390.jarcraft.init.ModBlocks;
 import com.ash6390.jarcraft.renderer.ItemRenderGlassLamp;
+import com.ash6390.jarcraft.renderer.ItemRenderIlluminatingOrb;
 import com.ash6390.jarcraft.renderer.RenderGlassLamp;
 import com.ash6390.jarcraft.tileentity.TileEntityGlassLamp;
+import com.ash6390.jarcraft.tileentity.TileEntityIlluminatingOrb;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.MinecraftForge;
 
 public class ClientProxy extends ServerProxy
 {
     public void registerRenderThings()
     {
         //GlassLamp
-        TileEntitySpecialRenderer render = new RenderGlassLamp();
-        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityGlassLamp.class, render);
-        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.glassLamp), new ItemRenderGlassLamp(render, new TileEntityGlassLamp()));
+        TileEntitySpecialRenderer renderGlassLamp = new RenderGlassLamp();
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityGlassLamp.class, renderGlassLamp);
+        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.glassLamp), new ItemRenderGlassLamp(renderGlassLamp, new TileEntityGlassLamp()));
+        //IlluminatingOrb
+        TileEntitySpecialRenderer renderIllOrb = new RenderGlassLamp();
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityIlluminatingOrb.class, renderIllOrb);
+        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.illuminatingOrbBlock), new ItemRenderIlluminatingOrb(renderIllOrb, new TileEntityGlassLamp()));
 
     }
 
