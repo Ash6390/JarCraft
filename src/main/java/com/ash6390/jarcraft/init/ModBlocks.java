1570cd496f6787c0e97776266ad746fa3459889b
Matteo <matteo.colombi259@gmail.com>
2014-10-17 22:48:18 +0200

    Added the IlluminatingOrbBlock

--- src/main/java/com/ash6390/jarcraft/init/ModBlocks.java
+++ src/main/java/com/ash6390/jarcraft/init/ModBlocks.java
@@ -3,7 +3,7 @@ package com.ash6390.jarcraft.init;
 import com.ash6390.jarcraft.blocks.BlockJC;
 import com.ash6390.jarcraft.blocks.GlassIngotBlock;
 import com.ash6390.jarcraft.blocks.GlassLamp;
-import com.ash6390.jarcraft.creativetab.CreativeTabJC;
+import com.ash6390.jarcraft.blocks.IlluminatingOrbBlock;
 import com.ash6390.jarcraft.reference.References;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
@@ -13,11 +13,13 @@ import net.minecraft.block.material.Material;
 public class ModBlocks
 {
     public static final BlockJC glassIngotBlock = new GlassIngotBlock();
-    public static final Block glassLamp = new GlassLamp(Material.glass).setLightLevel(1.0F).setBlockName("glassLamp").setHardness(0.3F).setResistance(0.3F).setCreativeTab(CreativeTabJC.JC_TAB).setBlockTextureName("glassLamp");
+    public static final Block glassLamp = new GlassLamp(Material.glass).setBlockName("glassLamp");
+    public static final Block illuminatingOrbBlock = new IlluminatingOrbBlock(Material.glass).setBlockName("illuminatingOrbBlock");
 
     public static void init()
     {
         GameRegistry.registerBlock(glassIngotBlock, "glassIngotBlock");
         GameRegistry.registerBlock(glassLamp, "glassLamp");
+        GameRegistry.registerBlock(illuminatingOrbBlock, "illuminatingOrbBlock");
     }
 }
