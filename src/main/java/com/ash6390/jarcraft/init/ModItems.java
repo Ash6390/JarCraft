5d8e524c2ba64504759d9e7b1f4db42aaa01714f
Matteo <matteo.colombi259@gmail.com>
2014-10-17 22:48:26 +0200

    Formatting

--- src/main/java/com/ash6390/jarcraft/init/ModItems.java
+++ src/main/java/com/ash6390/jarcraft/init/ModItems.java
@@ -8,11 +8,13 @@ public class ModItems
     public static final ItemJC illuminatingOrb = new IlluminatingOrb();
     public static final ItemJC glassIngot = new GlassIngot();
     public static final ItemJC glassStick = new GlassStick();
+    public static final ItemJC testItem = new TestItem();
 
     public static void init()
     {
         GameRegistry.registerItem(illuminatingOrb, "illuminatingOrb");
         GameRegistry.registerItem(glassIngot, "glassIngot");
         GameRegistry.registerItem(glassStick, "glassStick");
+        GameRegistry.registerItem(testItem, "testItem");
     }
 }
